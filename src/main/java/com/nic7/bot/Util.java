92852e449898f3b8e753e14c435ca1f7e166c36c
Nico <152223501+nicobota@users.noreply.github.com>
2026-02-20 15:12:55 -0800

    Test Commit

---
+++ src/main/java/com/nic7/bot/Util.java
@@ -0,0 +1,10 @@
+package com.nic7.bot;
+
+public class Util {
+
+    public static String SIG_NATION = "1199798159296176288";
+
+    public static boolean toggleBoolean(boolean b) {
+        return (b ? false : true);
+    }
+}
