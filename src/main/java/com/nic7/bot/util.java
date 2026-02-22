dcda5708f3e9a0cfca11fdac18ca5a8e37808fa7
nicob-37 <nicholasbota@gmail.com>
2026-02-21 23:20:48 -0800

    bugfix?

--- src/main/java/com/nic7/bot/util.java
+++ src/main/java/com/nic7/bot/util.java
@@ -4,8 +4,8 @@ public class util {
 
     public static double VERSION = 1.11;
 
-    public static boolean andyReply = true;
-    public static boolean aidenBruh = true;
+    public static boolean andyReply = false;
+    public static boolean aidenBruh = false;
 
     public static boolean toggleBoolean(boolean b) {
         return (b ? false : true);
