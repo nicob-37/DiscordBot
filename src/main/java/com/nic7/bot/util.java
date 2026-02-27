33702d7a380b8728f842da4f232bee4552eae70c
Nico <152223501+nicobota@users.noreply.github.com>
2026-02-27 09:44:36 -0800

    commit

--- src/main/java/com/nic7/bot/util.java
+++ src/main/java/com/nic7/bot/util.java
@@ -4,7 +4,7 @@ public class util {
 
     public static double VERSION = 1.11;
 
-    public static boolean andyReply = false;
+    public static boolean andyReply = true;
     public static boolean aidenBruh = false, andyBruh = false;
 
     public static boolean toggleBoolean(boolean b) {
