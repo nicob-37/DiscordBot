0451f5a6c61b0511585ab4f1291b1aa586e393fd
nicob-37 <nicholasbota@gmail.com>
2026-04-16 14:18:42 -0700

    thanks gemini

--- src/main/java/com/nic7/bot/manager/CommandsManager.java
+++ src/main/java/com/nic7/bot/manager/CommandsManager.java
@@ -2,13 +2,14 @@ package com.nic7.bot.manager;
 
 import com.nic7.bot.ID;
 import com.nic7.bot.util;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
-import net.dv8tion.jda.api.events.GatewayPingEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.IntegrationType;
+import net.dv8tion.jda.api.interactions.InteractionContextType;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
@@ -17,7 +18,10 @@ import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 import net.dv8tion.jda.api.utils.FileUpload;
 import org.jetbrains.annotations.NotNull;
 
-import javax.swing.text.html.Option;
+import java.awt.*;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,7 +31,6 @@ public class CommandsManager extends ListenerAdapter {
 
     public static boolean commandsEnabled = true;
 
-    // Initializes Commands On Bot Ready
     @Override
     public void onReady(@NotNull net.dv8tion.jda.api.events.session.ReadyEvent event) {
         initSlashCommandsEx(event);
@@ -41,12 +44,16 @@ public class CommandsManager extends ListenerAdapter {
 
         public SlashCommandEx(String name, String description, boolean protection, String authorizedID) {
             this.name = name; this.description = description; this.protection = protection; this.authorizedID = authorizedID;
-            this.data = Commands.slash(name, description);
+            this.data = Commands.slash(name, description)
+                    .setContexts(InteractionContextType.GUILD, InteractionContextType.BOT_DM, InteractionContextType.PRIVATE_CHANNEL)
+                    .setIntegrationTypes(IntegrationType.GUILD_INSTALL, IntegrationType.USER_INSTALL);
         }
 
         public SlashCommandEx(String name, String description) {
             this.name = name; this.description = description; this.protection = false;
-            this.data = Commands.slash(name, description);
+            this.data = Commands.slash(name, description)
+                    .setContexts(InteractionContextType.GUILD, InteractionContextType.BOT_DM, InteractionContextType.PRIVATE_CHANNEL)
+                    .setIntegrationTypes(IntegrationType.GUILD_INSTALL, IntegrationType.USER_INSTALL);
         }
 
         public SlashCommandEx addOption(OptionType type, String name, String desc, boolean required) {
@@ -58,7 +65,6 @@ public class CommandsManager extends ListenerAdapter {
             this.data.addOptions(options);
             return this;
         }
-
     }
 
     private boolean isAuthorized(SlashCommandInteraction event, String authorizedID) {
@@ -70,37 +76,31 @@ public class CommandsManager extends ListenerAdapter {
     }
 
     public void initSlashCommandsEx(@NotNull net.dv8tion.jda.api.events.session.ReadyEvent event) {
+        // Clear list to prevent duplicates if onReady fires twice
+        commands.clear();
+
         var guild = event.getJDA().getGuildById(ID.SIG_NATION);
 
         commands.add(new SlashCommandEx("toggle_commands", ".", true, ID.NICO));
-
         commands.add(new SlashCommandEx("view_toggles", "View which settings are on or off"));
-
         commands.add(new SlashCommandEx("stop", "Stops Bot", true, ID.NICO));
         commands.add(new SlashCommandEx("restart", "Restarts and checks for updates", true, ID.NICO));
         commands.add(new SlashCommandEx("version", "Get current bot version", true, ID.NICO));
-
         commands.add(new SlashCommandEx("toggle_andy_reply", "Toggles andy heartbreak", true, ID.NICO));
         commands.add(new SlashCommandEx("toggle_bruh", "Toggles bruh reactions", true, ID.NICO)
                 .addOption(OptionType.USER, "user", "User to toggle", true));
-
         commands.add(new SlashCommandEx("set_status", "Sets Nic7's status", true, ID.NICO)
                 .addOption(OptionType.STRING, "status", "Custom Status", true));
-
         commands.add(new SlashCommandEx("reddit_post", "Create a reddit post")
                 .addOption(OptionType.STRING, "title", "Title of post", true)
                 .addOption(OptionType.STRING, "body", "Body of post", true)
                 .addOption(OptionType.ATTACHMENT, "attachment", "Optional attachment", false));
-
         commands.add(new SlashCommandEx("ping", "pong"));
-
         commands.add(new SlashCommandEx("m", ".", true, ID.NICO)
                 .addOption(OptionType.STRING, "body", ".", true)
                 .addOption(OptionType.ATTACHMENT, "file", ".", false));
-
         commands.add(new SlashCommandEx("get_avatar", "gets a user's custom avatar")
                 .addOption(OptionType.USER, "user", ".", true));
-
         commands.add(new SlashCommandEx("randomize_status", ".", true, ID.NICO));
 
         OptionData pieceOption = new OptionData(OptionType.STRING, "piece", "The type of armor", true)
@@ -111,18 +111,20 @@ public class CommandsManager extends ListenerAdapter {
 
         OptionData hexOption = new OptionData(OptionType.STRING, "hex", "The Hex color (e.g. FFFFFF)", true);
 
-// Add the command to your list
         commands.add(new SlashCommandEx("generate_armor", "Generates leather armor")
                 .addOptions(pieceOption, hexOption));
 
         List<SlashCommandData> jdaData = new ArrayList<>();
+        for (SlashCommandEx ex : commands) { jdaData.add(ex.data); }
 
-        for (SlashCommandEx ex : commands) {jdaData.add(ex.data);}
-
+        // Sync to the specific guild
         if (guild != null) {
             guild.updateCommands().addCommands(jdaData).queue();
             System.out.println("Custom Commands Synced in Sigma Nation");
         }
+
+        // Also update Global Commands so User-Installable apps work everywhere
+        event.getJDA().updateCommands().addCommands(jdaData).queue();
     }
 
     @Override
@@ -145,30 +147,22 @@ public class CommandsManager extends ListenerAdapter {
         }
 
         if (event.getUser().getId().equals(ID.NICO) || commandsEnabled) {
-
             switch (event.getName()) {
-
                 case "toggle_commands" -> {
                     commandsEnabled = util.toggleBoolean(commandsEnabled);
                     event.reply("Commands are now " + (commandsEnabled ? "**True**" : "**False**")).setEphemeral(true).queue();
                 }
-
                 case "stop" -> {
                     event.reply("Shutting Down nic7").queue();
                     event.getJDA().shutdown();
                     System.exit(0);
                 }
-
                 case "restart" -> {
                     event.reply("Restarting and checking for update...").queue(success -> {
                         try {
-                            // setsid runs the script in its own session, independent of the bot
                             ProcessBuilder pb = new ProcessBuilder("setsid", "sh", "/home/ubuntu/DiscordBot/update_bot.sh");
                             pb.start();
-
-                            // Wait 1 second to ensure the script has actually started
                             Thread.sleep(1000);
-
                             event.getJDA().shutdown();
                             System.exit(0);
                         } catch (Exception e) {
@@ -176,55 +170,33 @@ public class CommandsManager extends ListenerAdapter {
                         }
                     });
                 }
-
-                case "ping" -> {
-                    event.reply("Pong").queue();
-                }
-
-                case "version" -> {
-                    event.reply("Version #" + util.VERSION).queue();
-                }
-
+                case "ping" -> event.reply("Pong").queue();
+                case "version" -> event.reply("Version #" + util.VERSION).queue();
                 case "set_status" -> {
                     String newActivity = event.getOption("status").getAsString();
-
                     event.getJDA().getPresence().setActivity(Activity.customStatus(newActivity));
                     event.reply("Status updated to: ** " + newActivity + "**").queue();
                 }
-
                 case "reddit_post" -> {
                     String postBody = event.getOption("body").getAsString();
                     String postTitle = event.getOption("title").getAsString();
                     var attachmentOption = event.getOption("attachment");
-
-                    var replyAction = event.reply(
-                            "# " + postTitle
-                                    + "\n"
-                                    + postBody
-                                    + "\n"
-                                    + "-# __" + "Post created by " + event.getUser().getName() + "__");
-
+                    var replyAction = event.reply("# " + postTitle + "\n" + postBody + "\n" + "-# __" + "Post created by " + event.getUser().getName() + "__");
                     if (attachmentOption != null) {
                         Message.Attachment attachment = attachmentOption.getAsAttachment();
                         replyAction.addFiles(FileUpload.fromData(attachment.getProxy().download().join(), attachment.getFileName()));
                     }
-
-                    replyAction.queue(hook -> {
-                        hook.retrieveOriginal().queue(message -> {
-                            message.addReaction(Emoji.fromCustom("updoot", 1474560551773536366L, false)).queue();
-                            message.addReaction(Emoji.fromCustom("downdoot", 1474560608346312936L, false)).queue();
-                        });
-                    });
+                    replyAction.queue(hook -> hook.retrieveOriginal().queue(message -> {
+                        message.addReaction(Emoji.fromCustom("updoot", 1474560551773536366L, false)).queue();
+                        message.addReaction(Emoji.fromCustom("downdoot", 1474560608346312936L, false)).queue();
+                    }));
                 }
-
                 case "toggle_andy_reply" -> {
                     util.andyReply = util.toggleBoolean(util.andyReply);
                     event.reply("Andy Reply is now " + util.andyReply).setEphemeral(true).queue();
                 }
-
                 case "toggle_bruh" -> {
                     var user = event.getOption("user").getAsUser().getId();
-
                     if (user.equals(ID.ANDY)) {
                         util.andyBruh = util.toggleBoolean(util.andyBruh);
                         event.reply("andyBruh is now " + util.andyBruh).setEphemeral(true).queue();
@@ -233,86 +205,54 @@ public class CommandsManager extends ListenerAdapter {
                         event.reply("aidenBruh is now " + util.aidenBruh).setEphemeral(true).queue();
                     } else {
                         event.deferReply().queue();
-                        return;
                     }
-
                 }
-
                 case "m" -> {
                     String message = event.getOption("body").getAsString();
                     var fileOption = event.getOption("file");
-
                     var sendAction = event.getChannel().sendMessage(message);
-
                     if (fileOption != null) {
                         var attachment = fileOption.getAsAttachment();
-                        sendAction.addFiles(FileUpload.fromData(
-                                attachment.getProxy().download().join(),
-                                attachment.getFileName()
-                        ));
+                        sendAction.addFiles(FileUpload.fromData(attachment.getProxy().download().join(), attachment.getFileName()));
                     }
-
                     sendAction.queue();
                     event.reply("Sent: **" + message + "**").setEphemeral(true).queue();
                 }
-
                 case "get_avatar" -> {
                     var user = event.getOption("user").getAsUser().getEffectiveAvatarUrl();
                     event.reply(user).queue();
                 }
-
-                case "randomize_status" -> {
-                    event.getJDA().getPresence().setActivity(Activity.customStatus(sm.randomStatus()));
-                }
-
+                case "randomize_status" -> event.getJDA().getPresence().setActivity(Activity.customStatus(sm.randomStatus()));
                 case "generate_armor" -> {
-                    // 1. Get and validate inputs
                     var pieceOpt = event.getOption("piece");
                     var hexOpt = event.getOption("hex");
                     if (pieceOpt == null || hexOpt == null) return;
 
                     String piece = pieceOpt.getAsString();
                     String hex = hexOpt.getAsString().replace("#", "");
-
-                    // Defer the reply because downloading an image can take a second
-                    // and we don't want the command to "time out"
                     event.deferReply().queue();
 
                     try {
-                        // 2. Build the URL
                         String urlString = "https://nico-armor-api.vercel.app/api/" + piece + "/" + hex;
-                        java.net.URL url = new java.net.URI(urlString).toURL();
-
-                        // 3. Download the image bytes
-                        try (java.io.InputStream in = url.openStream()) {
+                        URL url = new URI(urlString).toURL();
+                        try (InputStream in = url.openStream()) {
                             byte[] imageBytes = in.readAllBytes();
-
-                            // 4. Create the FileUpload
-                            // We name it "armor.png" so we can reference it in the embed
                             FileUpload file = FileUpload.fromData(imageBytes, "armor.png");
-
-                            // 5. Build the Embed
-                            net.dv8tion.jda.api.EmbedBuilder embed = new net.dv8tion.jda.api.EmbedBuilder()
+                            EmbedBuilder embed = new EmbedBuilder()
                                     .setTitle("Dye Result: " + piece.substring(0, 1).toUpperCase() + piece.substring(1))
-                                    .setColor(java.awt.Color.decode("0x" + hex))
-                                    // This magic string "attachment://filename" hides the URL!
+                                    .setColor(Color.decode("0x" + hex))
                                     .setImage("attachment://armor.png")
                                     .setFooter("Hex: #" + hex);
 
-                            // 6. Send the file and the embed together
-                            event.getHook().sendMessageEmbeds(embed.build())
-                                    .addFiles(file)
-                                    .queue();
+                            event.getHook().sendMessageEmbeds(embed.build()).addFiles(file).queue();
                         }
                     } catch (Exception e) {
                         event.getHook().sendMessage("Failed to generate armor: " + e.getMessage()).setEphemeral(true).queue();
                     }
                 }
-
             }
         } else {
             event.deferReply().queue();
         }
     }
-
 }
